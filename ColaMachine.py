993b2b23a9f18c8c871c9da62864eac9530c923e
reibegaj <89638299+reibegaj@users.noreply.github.com>
2022-07-06 15:14:44 +0200

    Added  Cola Machine app

---
+++ ColaMachine.py
@@ -0,0 +1,14 @@
+import sys
+
+print("Please choose one of the beverages: (1)Cola, (2) Sprite,(3) Pepsi,(4) Fanta,(5) Coffee)")
+
+beverages = {1: "Cola", 2: "Sprite", 3: "Pepsi", 4: "Fanta", 5: "Coffee"}
+
+x = int(input("Input your beverage code: "))
+
+if 1 <= x <= 5 :
+    print("Here's your: ", beverages.get(x))
+
+else:
+    print("Error. Choice was not valid,here is your money back.")
+    sys.exit(1)
