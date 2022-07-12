06272dfca725152c066da6af2401a7475568d491
reibegaj <89638299+reibegaj@users.noreply.github.com>
2022-07-12 15:36:13 +0200

    Added While Exercise

---
+++ While.py
@@ -0,0 +1,10 @@
+import sys
+
+x = int(input("Provide a number different than 5: "))
+
+while x != 5:
+    x = int(input("Provide a number different than 5: "))
+
+else:
+    print("Hey, you weren't supposed to enter 5")
+
