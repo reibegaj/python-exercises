bc09614a5d82112b61c4a3f6d9ba15f2196ac7f2
reibegaj <89638299+reibegaj@users.noreply.github.com>
2022-07-13 15:39:38 +0200

    Update PancakeGlutton.py
    According to 3rd requirement

--- PancakeGlutton.py
+++ PancakeGlutton.py
@@ -1,14 +1,17 @@
+from operator import itemgetter
+from collections import OrderedDict
+
 i = 1
 pancakes = {
-    "Person 1": int(), 
-    "Person 2": int(), 
+    "Person 1": int(),
+    "Person 2": int(),
     "Person 3": int(),
     "Person 4": int(),
     "Person 5": int(),
     "Person 6": int(),
-    "Person 7": int(), 
+    "Person 7": int(),
     "Person 8": int(),
-    "Person 9": int(),   
+    "Person 9": int(),
     "Person 10": int()
 }
 
@@ -21,3 +24,11 @@ for x in pancakes.keys():
 print("The person who ate the most pancakes is:",max(pancakes, key=pancakes.get))
 
 print("The person who ate the least pancakes is:",min(pancakes, key=pancakes.get))
+
+
+
+#OrderedDict = (sorted(pancakes.items(), key=itemgetter(1)))
+
+dict_sorted =sorted(pancakes.items(), key=lambda x: x[1])
+
+print(type(dict_sorted))
