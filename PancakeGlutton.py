ad26b50a3fd69fbd6b02282cb30f1fdc1822c7df
reibegaj <89638299+reibegaj@users.noreply.github.com>
2022-07-13 14:50:59 +0200

    Added PancakeGlutton.py
    According to second requirement

---
+++ PancakeGlutton.py
@@ -0,0 +1,23 @@
+i = 1
+pancakes = {
+    "Person 1": int(), 
+    "Person 2": int(), 
+    "Person 3": int(),
+    "Person 4": int(),
+    "Person 5": int(),
+    "Person 6": int(),
+    "Person 7": int(), 
+    "Person 8": int(),
+    "Person 9": int(),   
+    "Person 10": int()
+}
+
+for x in pancakes.keys():
+    print("Provide the number of pancakes eaten by Person",i)
+    pancakes[x] = input()
+    i = i + 1
+    #print(pancakes)
+
+print("The person who ate the most pancakes is:",max(pancakes, key=pancakes.get))
+
+print("The person who ate the least pancakes is:",min(pancakes, key=pancakes.get))
