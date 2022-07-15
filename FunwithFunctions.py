f1ca9fc4134c437ba8d93d8c863972624f6e63fa
reibegaj <89638299+reibegaj@users.noreply.github.com>
2022-07-15 12:03:57 +0200

    Added FunwithFunctions

---
+++ FunwithFunctions.py
@@ -0,0 +1,28 @@
+def say_hello(numri):
+    for numri in range(0, numri):
+        print('"Hello"')
+
+say_hello(10)
+
+
+def funx2(nr1,nr2):
+    print(nr1 + nr2)
+
+funx2(1,17)
+
+#i = int(3)
+#for i in range(0, i):
+#    say_hello(3)
+
+
+def half(nr):
+    print(nr)
+    while True:
+        if nr > 0:
+            nr = round(nr/2)
+            print(nr)
+        else: 
+            break 
+
+half(10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+input("Press Enter to Exit...")
